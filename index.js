50287de1bf31f493c67b190382bd776fde21b6f8
Lucas Bamonte Correa <bamontelucas@gmail.com>
2018-06-12 08:35:38 -0300

    corrected async load

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const response = await fetch('places.json');
-const json = await response.json();
+const load = async() => {
+    const response = await fetch('places.json');
+    const json = await response.json();
+    
+    const html = json.map(categoria => `
+        <li>
+            ${categoria.categoria}
+            <ul>
+                ${categoria.lugares.map(lugar => `<li>${lugar}</li>`)}
+            </ul>
+        </li>
+    `);
+    
+    document.querySelector('#places').insertAdjacentHTML('beforeend', html);
+}
 
-const html = json.map(categoria => `
-    <li>
-        ${categoria.categoria}
-        <ul>
-            ${categoria.lugares.map(lugar => `<li>${lugar}</li>`)}
-        </ul>
-    </li>
-`);
-
-document.querySelector('#places').insertAdjacentHTML('beforeend', html);
+load();
