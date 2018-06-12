0f0905655bb0af05805b70692733bb9a6fa60dc5
Lucas Bamonte Correa <bamontelucas@gmail.com>
2018-06-12 08:49:24 -0300

    bs cards

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const load = async() => {
     const json = await response.json();
     
     const html = json.map(categoria => `
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">${categoria.categoria}</h5>
-            </div>  
-            <ul class="list-group list-group-flush">
-                ${categoria.lugares.map(lugar => `<li class="list-group-item">${lugar}</li>`).join('')}
-            </ul>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${categoria.categoria}</h5>
+                </div>  
+                <ul class="list-group list-group-flush">
+                    ${categoria.lugares.map(lugar => `<li class="list-group-item">${lugar}</li>`).join('')}
+                </ul>
+            </div>
         </div>
     `).join('');
     
