963503a8df6422a3501c5e83741f52e3b17ec9ea
Lucas Bamonte Correa <bamontelucas@gmail.com>
2018-06-12 09:15:32 -0300

    added firebase

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const load = async() => {
-    const response = await fetch('places.json');
-    const json = await response.json();
-    
+const database = firebase.database();
+
+const load = async () => {
+    const snapshot = await database.ref('/').once('value');
+    const json = snapshot.val();
+
     const html = json.map(categoria => `
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <div class="card">
-                <div class="card-body">
+            <div class="card categoria">
+                <div class="card-header">
                     <h5 class="card-title">${categoria.categoria}</h5>
+                    <button type="button" class="btn btn-primary btn-add">+</button>
                 </div>  
                 <ul class="list-group list-group-flush">
                     ${categoria.lugares.map(lugar => `<li class="list-group-item">${lugar}</li>`).join('')}
@@ -14,8 +17,12 @@ const load = async() => {
             </div>
         </div>
     `).join('');
-    
+
     document.querySelector('#lugares').insertAdjacentHTML('beforeend', html);
+
+    document.querySelectorAll('.btn-add').forEach(button => {
+        button.addEventListener('click', () => alert('Funcionalidade ainda não desenvolvida'));
+    });
 }
 
 load();
